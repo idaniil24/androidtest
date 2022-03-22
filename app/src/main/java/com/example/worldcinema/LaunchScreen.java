58d73a10832ec42098b8f28ee0651ac63205349a
Wwwwwwwwww28 <kaimasida@mail.ru>
2022-03-22 16:37:38 +0600

    test

--- app/src/main/java/com/example/worldcinema/LaunchScreen.java
+++ app/src/main/java/com/example/worldcinema/LaunchScreen.java
@@ -2,7 +2,10 @@ package com.example.worldcinema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class LaunchScreen extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class LaunchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch_screen);
+
+        Thread logoTimer = new Thread()
+        {
+            public void run()
+            {
+                try {
+                    int logoTimer = 0;
+                    while (logoTimer < 5000)
+                    {
+                        sleep(100);
+                        logoTimer = logoTimer +100;
+                    };
+                    Intent i = new Intent(LaunchScreen.this, MainActivity.class);
+                    startActivity(i);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    finish();
+                }
+            }
+        };
+        logoTimer.start();
     }
 }
